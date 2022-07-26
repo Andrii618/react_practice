956b10dbdd5b45093c20802dc5e06831729fe80b
Sheva618 <applesheva618@gmail.com>
2022-07-26 18:25:52 +0300

    configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,7 @@ module.exports = {
     'import/no-extraneous-dependencies': 0,
     'arrow-body-style': 1,
     'prefer-arrow-callback': 1,
+    'react/no-unescaped-entities': 0,
   },
   settings: {
     'import/resolver': {
