55156f7792d15e8323275d2349f8fdab2752dcb5
Sheva618 <applesheva618@gmail.com>
2022-07-28 02:29:47 +0300

    component and props

---
+++ lesson3/task3/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Search from './Search';
+import './index.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<Search name="Tom" />);
