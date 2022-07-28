e216ea9d6761dd0af31dc5cc5ab7a59e381e13e8
Sheva618 <applesheva618@gmail.com>
2022-07-28 16:38:32 +0300

    components and props

---
+++ lesson3/task5/src/Profile.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import formatBirth from './formatBirth';
+
+import './profile.scss';
+
+const Profile = ({ userData }) => (
+  <div className="profile">
+    <div className="profile__name">
+      {userData.firstName} {userData.lastName}
+    </div>
+    <div className="profile__birth">
+      Was born {formatBirth(userData.birthDate)} in {userData.birthPlace}{' '}
+    </div>
+  </div>
+);
+
+export default Profile;
