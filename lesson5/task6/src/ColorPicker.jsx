ad78ff71f25cbe029d9ffdd68fb672451f27fe50
Sheva618 <applesheva618@gmail.com>
2022-07-29 15:21:58 +0300

    events

--- lesson5/task6/src/ColorPicker.jsx
+++ lesson5/task6/src/ColorPicker.jsx
@@ -3,29 +3,42 @@ import React, { Component } from 'react';
 import './colorPicker.scss';
 
 export default class Counter extends Component {
-  changeColorName(colorName) {
-    document.querySelector('.picker__title').textContent = colorName;
+  constructor() {
+    super();
+    this.state = { color: '' };
+  }
+
+  changeColor(color) {
+    this.setState({ color });
   }
 
   render() {
     return (
-      <div className="picker">
-        <div className="picker__title" onMouseOver={this.hideColorName}></div>
+      <div
+        className="picker"
+        onMouseOut={() => {
+          this.changeColor('');
+        }}
+      >
+        <div className="picker__title">{this.state.color}</div>
         <div>
           <button
             className="picker__button picker__button_coral"
-            onMouseOver={this.changeColorName.bind(this, 'Coral')}
-            onMouseOut={this.changeColorName.bind(this, '')}
+            onMouseEnter={() => {
+              this.changeColor('Coral');
+            }}
           ></button>
           <button
             className="picker__button picker__button_aqua"
-            onMouseOver={this.changeColorName.bind(this, 'Aqua')}
-            onMouseOut={this.changeColorName.bind(this, '')}
+            onMouseEnter={() => {
+              this.changeColor('Aqua');
+            }}
           ></button>
           <button
             className="picker__button picker__button_bisque"
-            onMouseOver={this.changeColorName.bind(this, 'Bisque')}
-            onMouseOut={this.changeColorName.bind(this, '')}
+            onMouseEnter={() => {
+              this.changeColor('Bisque');
+            }}
           ></button>
         </div>
       </div>
