bdeda7bfbaaddd2fa2eb2038ac6ba263dc48759c
Sheva618 <applesheva618@gmail.com>
2022-07-29 15:10:54 +0300

    events

--- lesson5/task6/src/ColorPicker.jsx
+++ lesson5/task6/src/ColorPicker.jsx
@@ -9,20 +9,23 @@ export default class Counter extends Component {
 
   render() {
     return (
-      <div className="picker" onMouseOut={this.changeColorName.bind(this, '')}>
+      <div className="picker">
         <div className="picker__title" onMouseOver={this.hideColorName}></div>
         <div>
           <button
             className="picker__button picker__button_coral"
             onMouseOver={this.changeColorName.bind(this, 'Coral')}
+            onMouseOut={this.changeColorName.bind(this, '')}
           ></button>
           <button
             className="picker__button picker__button_aqua"
             onMouseOver={this.changeColorName.bind(this, 'Aqua')}
+            onMouseOut={this.changeColorName.bind(this, '')}
           ></button>
           <button
             className="picker__button picker__button_bisque"
             onMouseOver={this.changeColorName.bind(this, 'Bisque')}
+            onMouseOut={this.changeColorName.bind(this, '')}
           ></button>
         </div>
       </div>
