76f5ce9ba8a4958c0bb4b18abf1b4c62f7273b84
Sheva618 <applesheva618@gmail.com>
2022-07-30 12:35:56 +0300

    conditional rendering

--- lesson6/task5/src/Auth.jsx
+++ lesson6/task5/src/Auth.jsx
@@ -40,3 +40,29 @@ class Auth extends Component {
   }
 }
 export default Auth;
+
+// const Auth = () => {
+//   const [project, setProject] = useState({ isLogged: false, isSpinner: false });
+
+//   const onLogin = () => {
+//     setProject({ ...project, isSpinner: true });
+
+//     setTimeout(() => {
+//       setProject({ isSpinner: false, isLogged: true });
+//     }, 2000);
+//   };
+
+//   const onLogout = () => {
+//     setProject({ ...project, isLogged: false });
+//   };
+
+//   return (
+//     <>
+//       {!project.isSpinner && project.isLogged && <Logout onLogout={onLogout} />}
+//       {!project.isSpinner && !project.isLogged && <Login onLogin={onLogin} />}
+//       {project.isSpinner && <Spinner size="50px" />}
+//     </>
+//   );
+// };
+
+// export default Auth;
