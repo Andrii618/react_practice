c12d2c0baaf75489f438177f0e1d1e372f775999
Sheva618 <applesheva618@gmail.com>
2022-07-30 15:17:43 +0300

    conditional rendering

--- lesson6/task6/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import NumbersList from './NumbersList';
-
-const numbers = [1, 2, 3, 4, 5];
-
-const App = () => <NumbersList numbers={numbers} />;
-
-export default App;
--- lesson6/task6/src/ListItem.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const ListItem = ({ elem }) => <li>{elem}</li>;
-
-export default ListItem;
