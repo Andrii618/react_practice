838f04c11278a3ac0e1871fab5ada532780c2ae7
Sheva618 <applesheva618@gmail.com>
2022-08-01 02:44:38 +0300

    lifecycle methods

---
+++ lesson8/task5/src/Offline.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './status.scss';
+
+const Offline = () => <div className="status status_offline">offline</div>;
+
+export default Offline;
---
+++ lesson8/task5/src/Online.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './status.scss';
+
+const Online = () => <div className="status">online</div>;
+
+export default Online;
