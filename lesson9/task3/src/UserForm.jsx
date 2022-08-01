eb5ee96d217496825333bf5d480404e5b655fafa
Sheva618 <applesheva618@gmail.com>
2022-08-01 20:41:35 +0300

    forms

---
+++ lesson9/task3/src/UserForm.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+import './form.scss';
+
+export default class UserForm extends Component {
+  setFormRef = node => {
+    console.log(node);
+    this.formRef = node;
+  };
+
+  render() {
+    return (
+      <form
+        ref={this.setFormRef}
+        className="login-form"
+        onSubmit={() => {
+          this.props.onSubmit(Object.fromEntries(new FormData(this.formRef)));
+        }}
+      >
+        <h1 className="form-title">Profile</h1>
+        <div className="form-control">
+          <label className="form-label" htmlFor="name">
+            Name
+          </label>
+          <input className="form-input" type="text" id="name" name="name" />
+        </div>
+        <div className="form-control">
+          <label className="form-label" htmlFor="student">
+            Student
+          </label>
+          <input className="form-input" type="checkbox" id="student" name="student" />
+        </div>
+        <div className="form-control">
+          <label className="form-label" id="occupation" htmlFor="occupation">
+            Occupation
+          </label>
+          <select name="occupation" className="form-input">
+            <option value="london">London</option>
+            <option value="new-york">New York</option>
+            <option value="sidney">Sidney</option>
+            <option value="berlin">Berlin</option>
+          </select>
+        </div>
+        <div className="form-control">
+          <label className="form-label" id="about" htmlFor="about">
+            About
+          </label>
+          <textarea name="about" className="form-input"></textarea>
+        </div>
+        <button className="submit-button" type="submit">
+          Submit
+        </button>
+      </form>
+    );
+  }
+}
