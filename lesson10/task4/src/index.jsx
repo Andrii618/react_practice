83c7fee0d83517c926fd474e266635a96d62f5cc
Sheva618 <applesheva618@gmail.com>
2022-08-03 00:16:50 +0300

    lifting state up

---
+++ lesson10/task4/src/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import FilterableProductTable from './FilterableProductTable';
+
+import './index.scss';
+
+const root = createRoot(document.getElementById('root'));
+
+const products = [
+  { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
+  { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
+  { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
+  { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
+  { category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
+  { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' },
+];
+
+root.render(<FilterableProductTable products={products} />);
