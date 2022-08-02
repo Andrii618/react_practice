dcdb5345788039af7a4c10610fcd29fe4b796c63
Sheva618 <applesheva618@gmail.com>
2022-08-02 19:26:30 +0300

    lifting state up

--- lesson10/task1/src/App.jsx
+++ lesson10/task1/src/App.jsx
@@ -6,37 +6,26 @@ import UserProfile from './UserProfile';
 import './index.scss';
 
 export default class App extends Component {
-  state = {
-    userData: null,
-  };
+  state = { userData: null };
 
   componentDidMount() {
-    this.fetchUserData(this.props.userName);
+    this.fetchUserData(this.props.userId);
   }
 
-  fetchUserData = name => {
-    fetch(`https://api.github.com/users/${name}`)
-      .then(res => res.json())
-      .then(data => {
-        if (data.message === 'Not Found') {
-          return null;
-        }
-
-        this.setState({ userData: data });
-      });
+  fetchUserData = userId => {
+    const userUrl = `https://api.github.com/users/${userId}`;
+    fetch(userUrl)
+      .then(response => response.json())
+      .then(userData => this.setState({ userData }));
   };
 
   render() {
-    const data = this.state.userData;
-
-    if (!data) {
-      return null;
-    }
-
     return (
       <div className="page">
-        <UserMenu userData={data} />
-        <UserProfile userData={data} />
+        <header className="header">
+          <UserMenu userData={this.state.userData} />
+        </header>
+        <UserProfile userData={this.state.userData} />
       </div>
     );
   }
