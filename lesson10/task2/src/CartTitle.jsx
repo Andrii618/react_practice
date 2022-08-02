fe2e8e8e9ce75d650b0ff360983c44dd509aeab4
Sheva618 <applesheva618@gmail.com>
2022-08-02 21:05:05 +0300

    lifting state up

--- lesson10/task2/src/CartTitle.jsx
+++ lesson10/task2/src/CartTitle.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-const CartTitle = ({ count, userName }) => (
-  <div className="cart-title">
-    {userName}, you added {count} items
-  </div>
+const CartTitle = ({ userName, count }) => (
+  <div className="cart-title">{`${userName}, you added ${count} items`}</div>
 );
 
 export default CartTitle;
