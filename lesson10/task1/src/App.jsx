881f34b422931c0a282f70afff02e047e91bfbb6
Sheva618 <applesheva618@gmail.com>
2022-08-02 19:21:57 +0300

    lifting state up

--- lesson10/task1/src/App.jsx
+++ lesson10/task1/src/App.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import Page from './Page';
+import UserMenu from './UserMenu';
+import UserProfile from './UserProfile';
 
-const App = () => <Page userName="facebookdsasda" />;
+import './index.scss';
 
-export default App;
+export default class App extends Component {
+  state = {
+    userData: null,
+  };
+
+  componentDidMount() {
+    this.fetchUserData(this.props.userName);
+  }
+
+  fetchUserData = name => {
+    fetch(`https://api.github.com/users/${name}`)
+      .then(res => res.json())
+      .then(data => {
+        if (data.message === 'Not Found') {
+          return null;
+        }
+
+        this.setState({ userData: data });
+      });
+  };
+
+  render() {
+    const data = this.state.userData;
+
+    if (!data) {
+      return null;
+    }
+
+    return (
+      <div className="page">
+        <UserMenu userData={data} />
+        <UserProfile userData={data} />
+      </div>
+    );
+  }
+}
