5ee173e6d788c98be9d07206965198ba58f752c9
Sheva618 <applesheva618@gmail.com>
2022-08-05 23:25:17 +0300

    practice

--- lesson12/src/index.js
+++ lesson12/src/index.js
@@ -1,6 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./styles.scss";
-import App from "./App.jsx";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import App from './App';
+
+import './index.scss';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(<App />);
