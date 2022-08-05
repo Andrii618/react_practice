972db4c133b8eb70873b93b503305f0a2a7e4087
Sheva618 <applesheva618@gmail.com>
2022-08-05 23:06:20 +0300

    practice

--- lesson12/src/TasksList.jsx
+++ lesson12/src/TasksList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Task from './Task.jsx';
-import CreateTaskInput from './CreateTaskInput.jsx';
-import { createTask, fetchTasksList, updateTask, deleteTask } from './tasksGateway.js';
+import Task from './Task';
+import CreateTaskInput from './CreateTaskInput';
+import { createTask, fetchTasksList, updateTask, deleteTask } from './tasksGateway';
 
 class TasksList extends React.Component {
   state = {
