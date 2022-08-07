4ef6f600a2072a76f3be25da96bfde16c878a13b
Sheva618 <applesheva618@gmail.com>
2022-08-07 15:57:54 +0300

    react router

---
+++ lesson13/task3/src/User.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const User = () => {
+  const { userName } = useParams();
+  const [userData, setUserData] = useState(null);
+
+  const fetchUserData = () => {
+    console.log(userName);
+    fetch(`https://api.github.com/users/${userName}`)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(`Failed to load user data`);
+        }
+
+        return res.json();
+      })
+      .then(data => {
+        const { avatar_url: avatar, name, location } = data;
+
+        setUserData({ avatar, name, location });
+      });
+  };
+
+  if (!userData) {
+    fetchUserData();
+    return null;
+  }
+
+  const { avatar, name, location } = userData;
+
+  return (
+    <div className="user">
+      <img alt="User Avatar" src={avatar} className="user__avatar" />
+      <div className="user__info">
+        <span className="user__name">{name}</span>
+        <span className="user__location">{location}</span>
+      </div>
+    </div>
+  );
+};
+
+export default User;
---
+++ lesson13/task3/src/Users.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Route, Link, useRouteMatch } from 'react-router-dom';
+
+import User from './User';
+
+const Users = () => {
+  const { url } = useRouteMatch();
+
+  return (
+    <div className="page__content">
+      <h1>Users</h1>
+      <ul className="navigation">
+        <li className="navigation__item">
+          <Link to={`${url}/github`}>Github</Link>
+        </li>
+        <li className="navigation__item">
+          <Link to={`${url}/facebook`}>Facebook</Link>
+        </li>
+      </ul>
+      <Route exact path={url}>
+        <span>Select a user please</span>
+      </Route>
+      <Route path={`${url}/:userName`}>
+        <User />
+      </Route>
+    </div>
+  );
+};
+
+export default Users;
