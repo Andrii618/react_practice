00d86fb30755313d91a144af7ae6122639909a44
Sheva618 <applesheva618@gmail.com>
2022-08-08 08:43:09 +0300

    react hooks

--- lesson14/task4/src/App.jsx
+++ lesson14/task4/src/App.jsx
@@ -1,28 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+
 import ConnectionStatus from './ConnectionStatus';
 
-const App = () => {
-  const [networkStatus, setNetworkStatus] = useState('online');
-
-  const handleOnlineStatus = () => {
-    setNetworkStatus('online');
-  };
-
-  const handleOfflineStatus = () => {
-    setNetworkStatus('offline');
-  };
-
-  useEffect(() => {
-    window.addEventListener('online', handleOnlineStatus);
-    window.addEventListener('offline', handleOfflineStatus);
-
-    return () => {
-      window.removeEventListener('online', handleOnlineStatus);
-      window.removeEventListener('offline', handleOfflineStatus);
-    };
-  });
-
-  return <ConnectionStatus isOnline={networkStatus} />;
-};
+const App = () => <ConnectionStatus />;
 
 export default App;
--- lesson14/task4/src/ConnectionStatus.jsx
+++ lesson14/task4/src/ConnectionStatus.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classNames from 'classnames';
 
-const ConnectionStatus = ({ isOnline }) => (
-  <div className={classNames('status', { status_offline: isOnline === 'offline' })}>
-    {isOnline === 'online' ? 'Online' : 'Offline'}
-  </div>
-);
+const ConnectionStatus = () => {
+  const [networkStatus, setNetworkStatus] = useState('online');
+
+  const handleOnlineStatus = () => {
+    setNetworkStatus('online');
+  };
+
+  const handleOfflineStatus = () => {
+    setNetworkStatus('offline');
+  };
+
+  useEffect(() => {
+    window.addEventListener('online', handleOnlineStatus);
+    window.addEventListener('offline', handleOfflineStatus);
+
+    return () => {
+      window.removeEventListener('online', handleOnlineStatus);
+      window.removeEventListener('offline', handleOfflineStatus);
+    };
+  });
+
+  return (
+    <div className={classNames('status', { status_offline: networkStatus === 'offline' })}>
+      {networkStatus === 'online' ? 'Online' : 'Offline'}
+    </div>
+  );
+};
 
 export default ConnectionStatus;
