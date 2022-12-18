191900ceae54bebef47aff180decacf1f4774371
GoItTutor <shevchuk.yt@gmail.com>
2022-12-18 13:46:01 +0200

    react intro

--- lesson1/todo-list/src/index.js
+++ lesson1/todo-list/src/index.js
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import './styles.css';
 
-const root = document.querySelector('#root');
+const root = createRoot(document.querySelector('#root'));
 
 const titleElem = <h1 className="title">Todo List</h1>;
 
