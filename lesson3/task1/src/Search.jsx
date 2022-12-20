413dd382084b57e88dda1dc9c27c83156f1db7f3
GoItTutor <shevchuk.yt@gmail.com>
2022-12-20 23:36:30 +0200

    components and props

--- lesson3/task1/src/Search.jsx
+++ lesson3/task1/src/Search.jsx
@@ -2,12 +2,10 @@ import './search.scss';
 
 const Search = ({ name }) => (
   <div className="search">
-    <h1 className="search__title">{`Hello, ${name}. What to search for you?`}</h1>
+    <h1 className="search__title">Hello, {name}. What to search for you?</h1>
     <div className="search__field">
-      <input className="search__input" type="text" />
-      <button className="search__button" type="button">
-        Search
-      </button>
+      <input type="text" className="search__input" />
+      <button className="search__button">Search</button>
     </div>
   </div>
 );
