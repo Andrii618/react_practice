414d2e254545630bfc46897a900d9614f946a807
GoItTutor <shevchuk.yt@gmail.com>
2022-12-20 23:31:53 +0200

    components and props

--- lesson3/task1/src/Search.jsx
+++ lesson3/task1/src/Search.jsx
@@ -3,9 +3,7 @@ const Search = ({ name }) => (
     <h1 className="search__title">{`Hello, ${name}. What to search for you?`}</h1>
     <div className="search__field">
       <input className="search__input" type="text" />
-      <button className="search__button" type="button">
-        Search
-      </button>
+      <button className="search__button">Search</button>
     </div>
   </div>
 );
--- lesson3/task1/src/index.jsx
+++ lesson3/task1/src/index.jsx
@@ -1,5 +1,7 @@
 import { createRoot } from 'react-dom/client';
+
 import Search from './Search';
+
 import './index.scss';
 
 const root = createRoot(document.getElementById('root'));
